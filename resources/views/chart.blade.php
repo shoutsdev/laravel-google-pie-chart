aa5ca41b490970ef7f93d6c7782189df3bb37698
tah33 <tahmedhera@gmail.com>
2022-05-13 12:19:54 +0600

    first commit

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -18,7 +18,6 @@
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
 
-    google.charts.load('current', {'packages':['bar']});
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);
 
